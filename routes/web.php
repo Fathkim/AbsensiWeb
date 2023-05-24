f31e85afda37944621fddf6529ca6404838db90e
Fathkim <fathanhakim2006@gmail.com>
2023-05-24 14:53:02 +0700

    create relation

--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,9 @@ Route::middleware(['PreventAdminAccess'])->group(function () {
     
 });
 
+// qrcode
+Route::get('/qrcode', 'QrCodeController@index');
+
 // Rute-rute yang memerlukan peran admin
 Route::middleware(['checkrole:admin'])->group(function () {
     
